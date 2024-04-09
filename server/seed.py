a78b77a78ac1f1c195b467a5673586bfcd3a3d21
neuman <owiroadams@gmail.com>
2024-04-09 13:52:25 +0300

    completed

--- server/seed.py
+++ server/seed.py
@@ -5,14 +5,18 @@ from app import app
 from models import db, Pet
 
 with app.app_context():
+    
+    Pet.query.delete()
 
     # Create an empty list
     pets = []
+    
 
     # Add some Pet instances to the list
     pets.append(Pet(name = "Fido", species = "Dog"))
     pets.append(Pet(name = "Whiskers", species = "Cat"))
     pets.append(Pet(name = "Hermie", species = "Hamster"))
+    pets.append(Pet(name = "Slither", species = "Snake"))
 
     # Insert each Pet in the list into the database table
     db.session.add_all(pets)
